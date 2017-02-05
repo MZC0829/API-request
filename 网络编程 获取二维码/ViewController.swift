5a06930e255e217661ffd4a825fcac721a8644c6
麦志超 <LEN@maizhichaodeMacBook-Pro.local>
2017-02-05 22:10:36 +0800

    Initial Commit

---
+++ 网络编程 获取二维码/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  网络编程 获取二维码
+//
+//  Created by 麦志超 on 17/2/5.
+//  Copyright (c) 2017年 mzc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
